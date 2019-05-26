77c018b3e6be23690610f953512c5a404ba0b671
mnavidad <mchristmas@yahoo.com>
2019-05-26 17:34:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cli-esri-map2-kr7zdc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cli-esri-map2-kr7zdc)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // Set our map properties
+  mapCenter = [-122.4194, 37.7749];
+  basemapType = 'satellite';
+  mapZoomLevel = 12;
+
+  mapLoadedEvent(status: boolean) {
+    console.log('The map has loaded: ' + status);
+  }
+}
